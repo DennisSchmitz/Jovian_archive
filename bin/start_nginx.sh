217ea4e453e56380d0a10df65ac8fa8a8bfc00e6
Florian <florian@niqex.com>
2019-05-17 10:38:18 +0200

    change backticks for compatibility

--- bin/start_nginx.sh
+++ bin/start_nginx.sh
@@ -6,21 +6,7 @@
 # changes
 # 22-11-2018, rv, from 8080 to port 8083 because jupyter notebook is using this port
 
-parse_yaml() {
-   local prefix=$2
-   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
-   sed -ne "s|^\($s\)\($w\)$s:$s\"\(.*\)\"$s\$|\1$fs\2$fs\3|p" \
-        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
-   awk -F$fs '{
-      indent = length($1)/7;
-      vname[indent] = $2;
-      for (i in vname) {if (i > indent) {delete vname[i]}}
-      if (length($3) > 0) {
-         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
-         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
-      }
-   }'
-}
+source bin/functions.sh
 
 eval $(parse_yaml profile/pipeline_parameters.yaml "config_")
 currentuser=$(whoami)
